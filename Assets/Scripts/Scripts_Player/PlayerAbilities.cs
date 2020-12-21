00f17da270cdd08c0ca55cdc5174c2ee366540ba
JennCA <jennifer.cicvarova@gmail.com>
2020-12-21 23:17:57 +0100

    added player-footsteps

--- Assets/Scripts/Scripts_Player/PlayerAbilities.cs
+++ Assets/Scripts/Scripts_Player/PlayerAbilities.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PlayerAbilities : MonoBehaviour
 {
     private PlayerMovement playerMovement;
+    private PlayerFootsteps playerFootsteps;
 
     private Transform viewRoot;
     private float heightStanding = 1.8f;
     private float heightCrouching = 1f;
+
+    private float volumeSprint = 1f;
+    private float volumeCrouch = 0.1f;
+    private float volumeWalkMin = 0.2f, volumeWalkMax = 0.6f;
+
+    private float walkDistanceFootsteps = 0.45f;
+    private float sprintDistanceFootsteps = 0.3f;
+    private float crouchDistanceFootsteps = 0.6f;
 
     private bool isCrouching;
 
@@ -17,10 +26,16 @@ public class PlayerAbilities : MonoBehaviour
     public float crouchSpeed = 2f;
 
 
+    void Start() {
+        playerFootsteps.minVolume = volumeWalkMin;
+        playerFootsteps.maxVolume = volumeWalkMax;
+        playerFootsteps.distanceFootsteps = walkDistanceFootsteps;
+    }
 
     void Awake() {
         playerMovement = GetComponent<PlayerMovement>();
         viewRoot = transform.GetChild(0); //get View Root
+        playerFootsteps = GetComponentInChildren<PlayerFootsteps>();
     }
 
     // Update is called once per frame
@@ -34,10 +49,18 @@ public class PlayerAbilities : MonoBehaviour
     void sprint() {
         if(Input.GetKeyDown(KeyCode.LeftShift) && !isCrouching) {
             playerMovement.speed = sprintSpeed;
+
+            playerFootsteps.distanceFootsteps = sprintDistanceFootsteps;
+            playerFootsteps.minVolume = volumeSprint;
+            playerFootsteps.maxVolume = volumeSprint;
         }
 
         if(Input.GetKeyUp(KeyCode.LeftShift) && !isCrouching) {
             playerMovement.speed = moveSpeed;
+
+            playerFootsteps.distanceFootsteps = walkDistanceFootsteps;
+            playerFootsteps.minVolume = volumeWalkMin;
+            playerFootsteps.maxVolume = volumeWalkMax;
         }
     }
 
@@ -47,10 +70,20 @@ public class PlayerAbilities : MonoBehaviour
             if(isCrouching) {
                 viewRoot.localPosition = new Vector3(0f, heightStanding, 0f);
                 playerMovement.speed = moveSpeed;
+
+                playerFootsteps.distanceFootsteps = walkDistanceFootsteps;
+                playerFootsteps.minVolume = volumeWalkMin;
+                playerFootsteps.maxVolume = volumeWalkMax;
+
                 isCrouching = false;
             } else {
                 viewRoot.localPosition = new Vector3(0f, heightCrouching, 0f);
                 playerMovement.speed = crouchSpeed;
+
+                playerFootsteps.distanceFootsteps = crouchDistanceFootsteps;
+                playerFootsteps.minVolume = volumeCrouch;
+                playerFootsteps.maxVolume = volumeCrouch;
+
                 isCrouching = true;
             }
         }
---
+++ Assets/Scripts/Scripts_Player/PlayerFootsteps.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    private AudioSource soundFootsteps;
+    private CharacterController characterController;
+
+    private float accumulatedDistance;
+
+    [SerializeField]
+    private AudioClip[] clipFootsteps;
+
+    [HideInInspector]
+    public float minVolume, maxVolume;
+
+    [HideInInspector]
+    public float distanceFootsteps;
+
+
+    void Awake() {
+        soundFootsteps = GetComponent<AudioSource>();
+        characterController = GetComponentInParent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        footstepSoundCheck();
+    }
+
+    void footstepSoundCheck() {
+        if(!characterController.isGrounded) {
+            return;
+        }
+
+        if(characterController.velocity.sqrMagnitude > 0) {
+            accumulatedDistance += Time.deltaTime; //calculate the distance we passed
+            if(accumulatedDistance > distanceFootsteps) {
+                soundFootsteps.volume = Random.Range(minVolume, maxVolume);
+                soundFootsteps.clip = clipFootsteps[Random.Range(0, clipFootsteps.Length)];
+                soundFootsteps.Play();
+
+                accumulatedDistance = 0f;
+            }
+        } else {
+            accumulatedDistance = 0f;
+        }
+    }
+}
