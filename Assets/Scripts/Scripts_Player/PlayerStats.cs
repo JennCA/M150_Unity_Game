1552a3f0e26b054c654bd92f4f7066bc1f009617
JennCA <jennifer.cicvarova@gmail.com>
2021-01-04 17:25:23 +0100

    added stamina/health UI with script

---
+++ Assets/Scripts/Scripts_Player/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField]
+    private Image healthStats, staminaStats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void displayHealthStats(float health) {
+        health /= 100f;
+        healthStats.fillAmount = health;
+    }
+
+    public void displayStaminaStats(float stamina) {
+        stamina /= 100f;
+        staminaStats.fillAmount = stamina;
+    }
+}
