94237a94352c4452894141cde956c44de07d6c0d
JennCA <jennifer.cicvarova@gmail.com>
2021-01-05 15:00:41 +0100

    added enemy sound effects

---
+++ Assets/Scripts/Scripts_Enemy/EnemySound.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip screamClip, deathClip;
+
+    [SerializeField]
+    private AudioClip[] attackClips;
+
+    void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playScreamSound() {
+        audioSource.clip = screamClip;
+        audioSource.Play();
+    }
+
+    public void playDeathSound() {
+        audioSource.clip = deathClip;
+        audioSource.Play();
+    }
+
+    public void playAttackSound() {
+        //pick random clip
+        audioSource.clip = attackClips[Random.Range(0, attackClips.Length)];
+        audioSource.Play();
+    }
+}
