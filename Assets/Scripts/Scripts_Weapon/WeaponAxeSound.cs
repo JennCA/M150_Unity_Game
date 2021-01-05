4df5bb748e063599cff7980f32d2d0cb40c17ecf
JennCA <jennifer.cicvarova@gmail.com>
2021-01-05 12:28:39 +0100

    added weapon sound effects

---
+++ Assets/Scripts/Scripts_Weapon/WeaponAxeSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAxeSound : MonoBehaviour
+{
+    [SerializeField] //shows private variable in the editor
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip[] slashSounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playSlashSound() {
+        //switch between the 3 axe-sounds
+        audioSource.clip = slashSounds[Random.Range(0, slashSounds.Length)];
+        audioSource.Play();
+    }
+}
