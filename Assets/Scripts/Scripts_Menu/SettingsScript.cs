5f502c210cc740a2f48e49dd649ee44ae6796830
JennCA <jennifer.cicvarova@gmail.com>
2021-01-07 09:19:13 +0100

    added menu scripts

---
+++ Assets/Scripts/Scripts_Menu/SettingsScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void mute() {
+        AudioListener.pause = !AudioListener.pause;
+
+    }
+
+    public void triggerButtonBehavior(int i) {
+		switch (i) {
+		default:
+		case(0):
+			SceneManager.LoadScene("MainMenu");	
+			break;
+		}
+
+	}
+}
